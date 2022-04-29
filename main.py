37047aa1db3d5f488b814d95895b367fa275152f
Aziz1019 <starinc1998@gmail.com>
2022-04-29 14:08:20 +0500

    Almost ready bot with template structure

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 """
-This is an echo bot.
-It echoes any incoming text messages.
+Pocket Dictionary
 """
-
 import logging
-
 from aiogram import Bot, Dispatcher, executor, types
+from oxfordLookUp import get_definitions
+from googletrans import Translator
+
+translator = Translator()
 
 API_TOKEN = '5399942772:AAEWUxt6j1YidYHi0RqfhFHPKjXfP7JO3eY'
 
@@ -27,11 +28,24 @@ async def send_welcome(message: types.Message):
 
 
 @dp.message_handler()
-async def echo(message: types.Message):
-    # old style:
-    # await bot.send_message(message.chat.id, message.text)
+async def tarjimon(message: types.Message):
+    lang = translator.detect(message.text).lang
+    if len(message.text.split()) > 2:
+        dest = 'uz' if lang == 'en' else 'en'
+        await message.answer(translator.translate(message.text, dest).text)
+    else:
+        if lang == 'en':
+            word_id = message.text
+        else:
+            word_id = translator.translate(message.text, dest='en').text
 
-    await message.answer(message.text)
+        lookup = get_definitions(word_id)
+        if lookup:
+            await message.reply(f"Word: {word_id} \nDefinitions:\n{lookup['definitions']}")
+            if lookup.get('audio'):
+                await message.reply_voice(lookup['audio'])
+        else:
+            await message.reply("Bunday so'z topilmadi")
 
 
 if __name__ == '__main__':
--- oxfordLookUp.py
+++ oxfordLookUp.py
@@ -1,17 +1,33 @@
+import requests
+
 app_id = "8a0159b2"
 app_key = "3ccee657c84498785dac6063f84f49d4"
 language = "en-gb"
 
-word_id = "orange"
-url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
 
-r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
-print(r.status_code)
+def get_definitions(word_id):
 
-res = r.json()
+    url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
 
-print(res['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0])
-print(res['results'][1]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0])
-print(res['results'][2]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0])
+    r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
 
-print(res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['audioFile'])
+    res = r.json()
+
+    if 'error' in res.keys():
+        return False
+
+    output = {}
+    senses = res['results'][0]['lexicalEntries'][0]['entries'][0]['senses']
+    definitions = []
+    for sense in senses:
+        definitions.append(f"👉 {sense['definitions'][0]}")
+    output['definitions'] = "\n".join(definitions)
+    if res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0].get('audioFile'):
+        output['audio'] = res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['audioFile']
+
+    return output
+
+if __name__ == '__main__':
+    from pprint import pprint as print
+    print(get_definitions("Great Britain"))
+    print(get_definitions("america"))
